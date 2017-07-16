9aab563fd38aa74816c82ec41d43d10afe8b0a3a
jliip51 <jliip51@gmail.com>
2017-07-16 15:10:13 -0400

    updated 7_16 3p

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '886040370890584064-1WPxhs95ARew1Kvy6MYCvN7b0fsdQMo',
   access_token_secret: 'FJKLNdmQuZHy8uI4IgB06QHju8a2kE8RixKhWpes8HiZa',
 }
+
+exports.spotifyKeys = {
+  id: 'e732f2c305c34f18a9db5a288631d785',
+  secret: 'e737646f41924c5eb3604d4773c0c552',
+}
--- liri.js
+++ liri.js
@@ -1,22 +1,144 @@
 var request = require("request");
 var twitter = require("twitter");
 var spotify = require("node-spotify-api");
-var twitterKeys = require("./keys.js");
-var action = process.argv[2];
+var fs = require("fs");
+var inquirer = require("inquirer");
+var keys = require("./keys.js");
 
-var clientTW = new twitter(twitterKeys.twitterKeys);
-var params = {screen_name: 'volcoder'};
+var command = process.argv[2];
+var userInput = process.argv;
+var inputArray = [];
 
-//add functions
-clientTW.get('statuses/user_timeline', paramsTW, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
+for (i = 3; i < userInput.length; i++) {
+  inputArray.push(userInput[i]);
+}
+
+var input = inputArray.join("+");
+
+//function to retreive data from Twitter API NPM and log results to console//
+function getTweets() {
+  var clientTW = new twitter(keys.twitterKeys);
+  var paramsTW = {
+    screen_name: 'volcoder'
+  };
+  //add functions
+  clientTW.get('statuses/user_timeline', paramsTW, function(error, tweets, response) {
+    if (error) {
+      return console.log('Error occurred: ' + error)
+    }
+    tweets.forEach(function(result) {
+      console.log("\n---------------------------------");
+      console.log("@" + result.user.screen_name + " Tweeted at " + result.created_at);
+      console.log(result.text);
+      console.log("---------------------------------");
+        
+    });
+  });
+};
+
+//function to retreive data from spotify API NPM and log results to console//
+function getSong() {
+  if (input === ""){
+    var q = "the+sign+ace+of+base";
+  } else {
+    q = input;
   }
-});
+  var clientSpotify = new spotify(keys.spotifyKeys);
+  var paramsSpotify = {
+    type: 'track',
+    query: q,
+    limit: 1,
+    offset: 0
+  }
 
-clientTW.post('statuses/update', {status: 'Learn to code...Save the World'}, function(error, tweet, response) {
-  if(error) throw error;
-  console.log(tweet);  // Tweet body.
-  console.log(response);  // Raw response object.
-});
-//switch statements with actions defined
+  clientSpotify.search(paramsSpotify, function(error, response) {
+    if (error) {
+      return console.log('Error occurred: ' + error);
+    }
+    var itemArr = response.tracks.items[0];
+
+    console.log("\nArtist Name: " + itemArr.artists[0].name);
+    console.log("Track Name: " + itemArr.name);
+
+    if (itemArr.preview_url != null) {
+      console.log("Preview Link: " + itemArr.preview_url);
+    } else {
+      console.log("Spotify Link: " + itemArr.external_urls.spotify);
+    }
+
+    console.log("Album: " + itemArr.album.name);
+  });
+};
+
+//function to retreive data from OMDB API via Request NPM and log results to console//
+function getMovie() {
+  if (input === ""){
+    var movieName = "mr+nobody";
+  } else {
+    movieName = input;
+  }
+
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+  request(queryUrl, function(error, response, body) {
+
+    if (error) {
+      return console.log('Error occurred: ' + error);
+    }
+    var result = JSON.parse(body);
+
+    console.log("\nMovie Title: " + result.Title);
+    console.log("Release Year: " + result.Year);
+
+    for (var i = 0; i < 2; i++) {
+      console.log(result.Ratings[i].Source + " Rating: " + result.Ratings[i].Value);
+    }
+    console.log("Produced In: " + result.Country);
+    console.log("Available Languages: " + result.Language);
+    console.log("Plot: " + result.Plot);
+    console.log("Actors: " + result.Actors);
+
+  });
+};
+
+//function to retreive data from random.txt file via fs NPM and call function based on random command to log results to console//
+function random(){
+  fs.readFile("random.txt", "utf8", function(error, result){
+    if (error) {
+      return console.log('Error occured: ' + error);
+    }
+    var randomArray = result.split(",");
+    var randomCmd = randomArray[0];
+    var randomInput = randomArray[1];
+    input = randomInput.split(" ").join("+");
+
+    switch (randomCmd){
+      case "spotify-this-song":
+      getSong();
+      break;
+
+      case "movie-this":
+      getMovie();
+      break;
+    }
+  });
+};
+
+//Conditional function to call command function based on user input//
+switch (command) {
+  case "my-tweets":
+    getTweets();
+    break;
+
+  case "spotify-this-song":
+    getSong();
+    break;
+
+  case "movie-this":
+    getMovie();
+    break;
+
+  case "do-what-it-says":
+    random();
+    break;
+}
